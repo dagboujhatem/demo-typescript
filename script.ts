16b542c9e5b60e85118eca445be3de1457c77e58
dagbouj hatem <dagboujhatem@gmail.com>
2022-11-05 13:26:18 +0100

    first commit

---
+++ script.ts
@@ -0,0 +1,23 @@
+// var  messages : Array<string> = ['hello', 'again'];
+
+// alert(messages);
+
+
+interface Product {
+    name: string,
+    price?: number
+}
+
+
+var p1 : Product ={
+    name: 'coka',
+    price: 2.5
+}
+
+var p2 : Product ={
+    name: 'coka'
+}
+
+// les annotations 
+// extends (heritage)
+// implements  (les interfaces)
